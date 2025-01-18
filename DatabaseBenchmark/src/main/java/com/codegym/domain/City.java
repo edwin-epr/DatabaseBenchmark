a400380a531b0fe7eb27a8fc3fbd22fe10eb1682
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-01-18 00:50:04 -0600

    Se logra la primera parte del proyecto, antes de escribir la parte de redis.

---
+++ DatabaseBenchmark/src/main/java/com/codegym/domain/City.java
@@ -0,0 +1,23 @@
+package com.codegym.domain;
+
+import jakarta.persistence.*;
+import lombok.Setter;
+
+@Setter
+@Entity
+@Table(schema = "world", name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private String district;
+
+    private Integer population;
+}
---
+++ DatabaseBenchmark/src/main/java/com/codegym/domain/CountryLanguage.java
@@ -0,0 +1,35 @@
+package com.codegym.domain;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Type;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(schema = "world", name = "country_language")
+public class CountryLanguage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private String language;
+
+    @Column(name = "is_official", columnDefinition = "BIT")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private Boolean isOfficial;
+
+    private BigDecimal percentage;
+}
