634edd5d3e590b20dc07c55c8448c40cf33d8ad6
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-01-18 23:02:08 -0600

    Add hibernate.properties file to the project.

--- DatabaseBenchmark/src/main/java/com/codegym/Main.java
+++ DatabaseBenchmark/src/main/java/com/codegym/Main.java
@@ -17,8 +17,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
-import org.hibernate.query.Query;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -75,21 +76,11 @@ public class Main {
 
     private SessionFactory prepareRelationalDb() {
         final SessionFactory sessionFactory;
-        Properties properties = new Properties();
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
-        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/world");
-        properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "root");
-        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-        properties.put(Environment.HBM2DDL_AUTO, "validate");
-        properties.put(Environment.STATEMENT_BATCH_SIZE, "100");
 
         sessionFactory = new Configuration()
                 .addAnnotatedClass(City.class)
                 .addAnnotatedClass(Country.class)
                 .addAnnotatedClass(CountryLanguage.class)
-                .addProperties(properties)
                 .buildSessionFactory();
         return sessionFactory;
     }
